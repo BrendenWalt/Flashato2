373c50aee4b019259b49a643caab57e2511f7e12
BrendenWalt <brendenwalters42@gmail.com>
2016-11-07 21:57:23 -0800

    Added gulp, bourbon/neat.  Styling the landing page.

---
+++ Flashato/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding ProjectOpened='watch-sass' />
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass-compile', function () {
+    gulp.src('./Content/Styles/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./Content/Styles'));
+});
+
+gulp.task('watch-sass', function () {
+    gulp.watch('./Content/Styles/*.scss', ['sass-compile']);
+});
